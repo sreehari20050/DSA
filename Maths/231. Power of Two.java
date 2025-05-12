f3a4809fbb23f63b3c284c2e5218deb59dbffadd
Sreehari K B <172391114+sreehari20050@users.noreply.github.com>
2025-05-12 16:59:00 +0530

    Update 231. Power of Two.java

--- Maths/231. Power of Two.java
+++ Maths/231. Power of Two.java
@@ -10,3 +10,22 @@ class Solution {
         return false;
     }
 }
+
+
+//solution 2
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){
+            return false;
+        }
+        while(n != 1){
+            if(n % 2 != 0){
+                return false;
+            }
+            else{
+                n = n / 2;
+            }
+        }
+        return true;
+    }
+}
