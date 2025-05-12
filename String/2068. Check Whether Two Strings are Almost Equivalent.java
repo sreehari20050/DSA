e8b9c1bb5bb956cb354495a9fcb53fd0048dd608
Sreehari K B <172391114+sreehari20050@users.noreply.github.com>
2025-05-12 17:11:30 +0530

    Update 2068. Check Whether Two Strings are Almost Equivalent.java

--- String/2068. Check Whether Two Strings are Almost Equivalent.java
+++ String/2068. Check Whether Two Strings are Almost Equivalent.java
@@ -1,3 +1,4 @@
+//Solution 1
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
         int arr[] = new int[26];
@@ -5,6 +6,24 @@ class Solution {
             arr[word1.charAt(i) - 'a']++;
         }
         for(int i = 0 ; i < word2.length() ; i++){
+            arr[word2.charAt(i) - 'a']--;
+        }
+        for(int i = 0 ; i < arr.length ; i++){
+            if(Math.abs(arr[i]) > 3){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//Solution 2
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int arr[] = new int[26];
+        for(int i = 0 ; i < word1.length() ; i++){
+            arr[word1.charAt(i) - 'a']++;
             arr[word2.charAt(i) - 'a']--;
         }
         for(int i = 0 ; i < arr.length ; i++){
